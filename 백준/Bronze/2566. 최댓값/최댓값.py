aa4db89d1fe779a20ffaf800b06bb9d1141abb3a
shinjong kim <63417540+Spielmannkim@users.noreply.github.com>
2023-02-05 14:33:59 +0900

    [Bronze III] Title: 최댓값, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -0,0 +1,16 @@
+a=[]
+for _ in range(9):
+    a.append(list(map(int, input().split())))
+x = 0
+y = 0
+max = -1
+
+for i in range(9):
+    for j in range(9):
+        if a[i][j] > max:
+            max = a[i][j]
+            x = i+1
+            y = j+1
+            
+print(max)
+print(x,y)
